f27922532b45ffc68a987e864c55e3f6c9f9d6d8
rahul_tiwari30 <rahul.tiwari30091999@gmail.com>
2026-03-18 12:07:14 +0530

    UI UX improvents3

--- frontend/src/pages/AdminConsole.js
+++ frontend/src/pages/AdminConsole.js
@@ -2,7 +2,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { useEffect, useState } from "react";
 
 
-const { user } = useAuth0();
+
 
 function AdminConsole() {
   const { user } = useAuth0();
