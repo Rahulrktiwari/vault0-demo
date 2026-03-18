b3ddd9300ccae9cde27d790dfa86a00805080a9d
rahul_tiwari30 <rahul.tiwari30091999@gmail.com>
2026-03-18 12:11:29 +0530

    current user4

--- frontend/src/pages/AdminConsole.js
+++ frontend/src/pages/AdminConsole.js
@@ -2,10 +2,10 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { useEffect, useState } from "react";
 
 
-
+const { user } = useAuth0();
 
 function AdminConsole() {
-  const { user } = useAuth0();
+  
   console.log(user);
 
   const roles = user?.["https://vault0.com/roles"] || [];
